4a93703f8a64528ce30cc57898f307a11cf0a528
mvraux <45971463+mvraux@users.noreply.github.com>
2019-03-27 14:29:56 +0100

    Create databasedatas.sql
    Données factices

---
+++ DB/databasedatas.sql
@@ -0,0 +1,70 @@
+/* 
+ * Base de données factice pour phase test de la conception d'IHM
+ */
+/**
+ * Author:  Vraux Maxence
+ * Created: 18 févr. 2019
+ */
+
+
+use imp3d;
+
+/*FABLAB*/
+
+insert into fablab ( Nom,MinTemperature,MaxTemperature,MinHumidite,MaxHumidite) values
+    ("FabLab_Victor_Hugo",15,35,30,70);
+
+/*OPERATEUR DE BASE*/
+
+insert into operateur (FabLabNom, Nom, Prenom, MotDePasse, Mail ) values
+    ("FabLab_Victor_Hugo", "Dumas", "Jean pierre", "vhimp3d2019", "operateur@gmail.com" );
+
+/*IMPRIMANTE*/
+
+insert into imprimante3d (Nom,FabLabNom,NbHeuresDeTravail,Etat,DureeRestante,CoutHoraire) values
+    ("uprint1","FabLab_Victor_Hugo",1500,"REPOS",22,800);
+
+/*UTILISATEURS*/
+insert into utilisateur (Code, FabLabNom,Nom, Prenom, MotDePasse, Mail ,Etablissement ,DateInscription , NbJobsRealises ,NbEchecs , MailValide ) values
+    ("SDQ5S7GFS98S","FabLab_Victor_Hugo","Millet","Bertrand","mdpuser","user1@gmail.com","Lycée Bellevue","2018-05-22",2,1,TRUE);
+
+insert into utilisateur (Code, FabLabNom,Nom, Prenom, MotDePasse, Mail ,Etablissement ,DateInscription , NbJobsRealises ,NbEchecs , MailValide ) values
+    ("45A7W4D73","FabLab_Victor_Hugo","Lobe","Julie","mdpuser","user2@gmail.com","Rive gauche","2019-01-15",1,0,FALSE);
+
+/*JOB*/
+
+insert into job (Imprimante3dNom, UtilisateurID, Nom, DateRealisation , Etat, DureeConsommee, ResteAFaireEstimee, SupportConsomme, MatiereConsommee, SupportEstime, MatiereEstimee, Prix) values
+    ("uprint1","45A7W4D73","poignée","2019-01-17","FINI",18,0,40,12,0,0,14400);
+
+insert into job (Imprimante3dNom, UtilisateurID, Nom, DateRealisation , Etat, DureeConsommee, ResteAFaireEstimee, SupportConsomme, MatiereConsommee, SupportEstime, MatiereEstimee, Prix) values
+    ("uprint1","SDQ5S7GFS98S","cale","2019-01-17","FINI",5,0,15,12,0,0,720);
+
+insert into job (Imprimante3dNom, UtilisateurID, Nom, DateRealisation , Etat, DureeConsommee, ResteAFaireEstimee, SupportConsomme, MatiereConsommee, SupportEstime, MatiereEstimee, Prix) values
+    ("uprint1","45A7W4D73","vis","2019-01-05","FINI",2,0,9,7,0,0,360);
+
+insert into job (Imprimante3dNom, UtilisateurID, Nom, DateRealisation , Etat, DureeConsommee, ResteAFaireEstimee, SupportConsomme, MatiereConsommee, SupportEstime, MatiereEstimee, Prix) values
+    ("uprint1","SDQ5S7GFS98S","pieceqq","2019-01-17","FINI",3,0,10,9,0,0,450);
+
+/*CODES*/
+
+insert into codenouveau (Code, FabLabNom) values
+    ("48876Q9SD4","FabLab_Victor_Hugo");
+
+insert into codenouveau (Code, FabLabNom) values
+    ("887SDQ657S","FabLab_Victor_Hugo");
+
+/*CARTOUCHE*/
+
+insert into cartouche (Imprimante3DNom, DateRemplacement, NumeroDeSerie, QuantiteRestante, CoutAuMetre) values
+    ("uprint1","2019-01-01","CARTEST4991-SUP",487,1500);
+insert into cartouche (Imprimante3DNom, DateRemplacement,DateFabrication, NumeroDeSerie, QuantiteRestante, CoutAuMetre) values
+    ("uprint1","2019-01-01","2018-12-01","CARTEST4991-MAT",487,1500);
+
+/*AMBIANCE*/
+
+insert into ambiance (FabLabNom, Temperature, Humidite, Datation) values
+    ("FabLab_Victor_Hugo",25,50,"2019-02-18");
+
+insert into ambiance (FabLabNom, Temperature, Humidite, Datation) values
+    ("FabLab_Victor_Hugo",12,70,"2019-01-12");
+
